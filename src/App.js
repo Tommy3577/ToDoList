2dcd26370963fb6131d5a6d0aa7c3978a3a6c933
Tommy3577 <101331469+Tommy3577@users.noreply.github.com>
2022-07-07 17:13:45 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// class App2 extends React.Component{
+//   render({
+//     return(
+//       <div>
+//         <h1>I'm a class component</h1>
+//       </div>
+//     )
+//   })
+// }
+
+// let app3 = () => {
+//   return (
+//   <div>
+//      <person />
+//     <h1>I'm a class component</h1>
+//   </div>
+// )
+// }
+
+// export default App, App2, App3;
+
+// export let person = () => {
+//   return <h2>something</h2>
+// }
+
+// class App extends React.Component {
+//   render() {
+//   return (
+//   <div>
+//   <h1>I'm a class component</h1>
+//   <Person name="Dan" age="34" />
+//   <Person name="Stuart" age="30-something" />
+//   <Person name="Ben" age="21" />
+//   </div>
+//   );
+//   }
+//  }
+//  const Person = (props) => {
+//   return (
+//   <p> Hi I'm {props.name} and I'm {props.age} </p>
+//   );
+//  };
+
+// class App extends React.Component {
+ let App = () => {
+  return <h2></h2>
 }
+//   render() {
+//   return (
+//   <div>
+//   <h1>I'm a class component</h1>
+//   <Person name="Dan" age="34" />
+//   <Person name="Stuart" age="30-something" />
+//   <Person name="Ben" age="21" petsName="john" type="dog" />
+//   </div>
+//   );
+//   }
+//  }
+//  const Person = props => {
+//   return (
+//   <div>
+//   <p>Hi I'm {props.name} and I'm {props.age}</p>
+//   <Pet pet={props.petsName} />
+//   </div>
+//   );
+//  };
+//  const Pet = props => {
+//   return <p>Hi my pets name is {props.pet}</p>;
+//  };
 
-export default App;
+export default App
